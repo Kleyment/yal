25b17a7c508450190ab187eb8f710ce6256e85bc
Clement Bellanger <bellange2u@fst-ats-311-l17.fst.site.univ-lorraine.fr>
2018-01-24 14:51:34 +0100

    Verifier de Binaire Logique

--- arbre/expression/BinaireLogique.java
+++ arbre/expression/BinaireLogique.java
@@ -1,5 +1,7 @@
 package yal.arbre.expression;
 
+import yal.exceptions.AnalyseSemantiqueException;
+
 /**
  * 3 déc. 2015
  *
@@ -11,5 +13,22 @@ public abstract class BinaireLogique extends Binaire {
     protected BinaireLogique(Expression gauche, Expression droite) {
         super(gauche, droite) ;
     }
+    
+    @Override
+	public void verifier() {
+		if (gauche.getType() != BOOLEEN) {
+			StringBuilder erreur = new StringBuilder();
+			erreur.append("L'opérande gauche doit être booléenne : ");
+	    	erreur.append(gauche.getType());
+	    	
+	    	throw new AnalyseSemantiqueException(getNoLigne(), erreur.toString());	
+		} else if (droite.getType() != BOOLEEN) {
+			StringBuilder erreur = new StringBuilder();
+			erreur.append("L'opérande droite doit être booléenne : ");
+	    	erreur.append(droite.getType());
+	    	
+	        throw new AnalyseSemantiqueException(getNoLigne(), erreur.toString());	
+		}
+	}
     
 }

bd9a4f3800d945983c8bef1208720af0146d1c58
Highnewb <thouveninvalentin@gmail.com>
2018-01-24 14:50:05 +0100

    Ajout de superieur et inferieur

--- arbre/expression/Inferieur.java
+++ arbre/expression/Inferieur.java
@@ -16,5 +16,35 @@ public class Inferieur extends Comparaison {
     public String operateur() {
         return " < ";
     }
+
+	@Override
+	public int getType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void verifier() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toMIPS() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("# Inferieur\n");
+		Moins gMoinsd = new Moins(gauche, droite);
+		sb.append(gMoinsd.toMIPS());
+		sb.append("# Comparaison du resultat avec 0\n");
+		sb.append("bltz $v0, alors\n");
+		sb.append("# Si c'est inferieur a 0, on met 0 dans $v0");
+		sb.append("li $v0, 0\n");
+		sb.append("j fin\n");
+		sb.append("# Si c'est superieur a 0, on met 1 dans $v0");
+		sb.append("alors\n");
+		sb.append("li $v0, 1\n");
+		sb.append("fin\n");
+		return sb.toString();
+	}
     
 }

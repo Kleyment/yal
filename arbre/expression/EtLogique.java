9ab07e067b39c759adb42423b763f726d646e83c
Clement Bellanger <bellange2u@fst-ats-311-l17.fst.site.univ-lorraine.fr>
2018-01-24 14:39:35 +0100

    Début de ET et OU

--- arbre/expression/EtLogique.java
+++ arbre/expression/EtLogique.java
@@ -17,5 +17,26 @@ public class EtLogique extends BinaireLogique {
     public int getType() {
         return BOOLEEN;
     }
+    
+    @Override
+	public String toMIPS() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("# Et logique\n");
+		sb.append("# Calcul de la partie gauche\n");
+		sb.append(gauche.toMIPS());
+		sb.append("# Empilement de la partie gauche\n");
+		sb.append("sw $v0, 0($sp)");
+		sb.append("add $sp, $sp, -4");
+		sb.append("# Calcul de la partie droite\n");
+		sb.append(droite.toMIPS());
+		sb.append("# Dépilement de la partie gauche\n");
+		sb.append("add $sp, $sp, 4");
+		sb.append("lw $t8,($sp)");
+		sb.append("# Et logique entre $v0 et $t8\n");
+		sb.append("and $v0, $t8, $v0");
+		
+		return sb.toString();
+	}
   
 }
--- arbre/expression/OuLogique.java
+++ arbre/expression/OuLogique.java
@@ -17,4 +17,39 @@ public class OuLogique extends BinaireLogique {
         return " ou " ;
     }
 
+	@Override
+	public int getType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void verifier() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	 @Override
+	public String toMIPS() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("# Ou logique\n");
+		sb.append("# Calcul de la partie gauche\n");
+		sb.append(gauche.toMIPS());
+		sb.append("# Empilement de la partie gauche\n");
+		sb.append("sw $v0, 0($sp)");
+		sb.append("add $sp, $sp, -4");
+		sb.append("# Calcul de la partie droite\n");
+		sb.append(droite.toMIPS());
+		sb.append("# Dépilement de la partie gauche\n");
+		sb.append("add $sp, $sp, 4");
+		sb.append("lw $t8,($sp)");
+		sb.append("# Ou logique entre $v0 et $t8\n");
+		sb.append("or $v0, $t8, $v0");
+		
+		return sb.toString();
+	}
+    
+   
+
 }

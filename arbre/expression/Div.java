aa87e71ffde3058af50fc66d53bf7343c93e4a73
Pierre Génard <pierre.genard4@etu.univ-lorraine.fr>
2018-01-25 11:00:33 +0100

    correction Mult

--- arbre/expression/Div.java
+++ arbre/expression/Div.java
@@ -28,28 +28,28 @@ public class Div extends BinaireArithmetique {
 		sb.append("# Calcul de la partie gauche\n");
 		sb.append(gauche.toMIPS());
 		sb.append("# Empilement de la partie gauche\n");
-		sb.append("sw $v0,0($sp)\n");
+		sb.append("sw $v0, 0($sp)\n");
 		sb.append("add $sp, $sp, -4\n");
 		
 		sb.append("# Calcul de la partie droite\n");
 		sb.append(droite.toMIPS());
 		sb.append("# Dépilement de la partie gauche\n");
 		sb.append("add $sp, $sp, 4\n");
-		sb.append("lw $t8,($sp)\n");
+		sb.append("lw $t8, ($sp)\n");
 		
 		sb.append("# Gestion de la division par 0\n");
 		sb.append("beqz $v0, alors_" + this.hashCode() + "\n");
 		
 		sb.append("# Division entre $v0 et $t8 -> $v0\n");
 		sb.append("div $v0, $t8, $v0\n");
-		sb.append("j fin_"+this.hashCode()+"\n");
-		sb.append("alors_" + this.hashCode() + ":\n");
+		sb.append("j fin_" + this.hashCode() + "\n");
+		sb.append("alors_" + this.hashCode() + " :\n");
 		sb.append("# Message d'erreur car l'expression droite est egale a 0\n");
 		sb.append("li $v0, 4\n");
 		sb.append("la $a0, err_div\n");
 		sb.append("syscall\n");
 		sb.append("j end\n");
-		sb.append("fin_"+this.hashCode()+":\n");
+		sb.append("fin_" + this.hashCode() + " :\n");
 				
 		return sb.toString();
 	}

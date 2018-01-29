7865cff1bf679edec71b36cc2c2b21bdf6eb2bbb
Pierre Génard <pierre.genard4@etu.univ-lorraine.fr>
2018-01-29 09:27:43 +0100

    Documentation 4

--- README.md
+++ README.md
@@ -3,6 +3,23 @@
 ## Auteurs
 Clément Bellanger, Pierre Génard, Valentin Thouvenin
 
+## Des tests
+```bash
+#!/bin/bash
+
+for i in `seq 30`; do sep="$sep="; done
+
+for yal in $(ls *.yal)
+do
+  echo $sep
+  echo $yal
+  java -jar yal.jar $yal
+  [ $? -eq 0 ] && java -jar Mars4_5.jar dec v1  ${yal:${#yal}-4:3}.mips # ${yal//yal/mips}
+done
+
+exit 0
+```
+
 ## Assembleur MIPS
 - [Télécharger MARS](http://courses.missouristate.edu/KenVollmar/MARS/download.htm)
 - [Utiliser MARS en ligne de commande](https://courses.missouristate.edu/KenVollmar/mars/Help/MarsHelpCommand.html)
--- Yal.java
+++ Yal.java
@@ -22,7 +22,7 @@ public class Yal {
             AnalyseurSyntaxique analyseur = new AnalyseurSyntaxique(new AnalyseurLexical(new FileReader(source)));
             ArbreAbstrait arbre = (ArbreAbstrait) analyseur.parse().value;
                     
-            /* Vérification du code */
+            /* Vérification de l'arbre abstrait */
             arbre.verifier(); 
             System.out.println("COMPILATION OK");
             
@@ -30,56 +30,76 @@ public class Yal {
             StringBuilder code = new StringBuilder(200);
             
             code.append(".data\n");
-            code.append("err_div:\t.asciiz \"ERREUR EXECUTION :\\n\\t division par zéro\"\n");
+            code.append("err_div:\t.asciiz \"ERREUR EXECUTION :\\n\\t division par zéro\"\n\n");
             code.append(".text\n");
             code.append("main :\n");
             code.append("# initialiser s7 avec sp (initialisation de la base des variables)\n");
             code.append("move $s7, $sp\n");
             code.append(arbre.toMIPS());
-            code.append("end :\n");
+            code.append("\nend :\n");
             code.append("# fin du programme\n");
-            code.append("move $v1, $v0\t# copie de v0 dans v1 pour permettre les tests de yal0\n");
+            code.append("move $v1, $v0\t# copie de $v0 dans $v1 pour permettre les tests de yal0\n");
             code.append("li $v0, 10\t# retour au système\n");
             code.append("syscall\n");
-    
-            /* Crée le nom du fichier de sortie */
-            StringBuilder sortie = new StringBuilder(10);
-            int suffixe = source.lastIndexOf('.');
             
-            if (suffixe < 1) {
-            	sortie.append(source);
-            }
-            else {
-            	sortie.append(source.substring(0, suffixe));
-            }
-            
-            sortie.append(".mips");
-            
-            /* Ecriture du code MIPS */
-            try {
-            	FileWriter flot = new FileWriter(sortie.toString());
-            	BufferedWriter tampon = new BufferedWriter(flot);
-            	
-                tampon.write(code.toString());
-            	
-                tampon.close();
-    		    flot.close();          
-            }
-            catch (IOException e) { 
-            	System.err.println("Erreur lors de l'écriture du code MIPS dans le fichier " + sortie);
-            }
+            /* Ecriture du code */
+            ecriture(code.toString(), sortie(source));
         } 
         catch (FileNotFoundException ex) {
-            System.err.println("Fichier " + source + " inexistant") ;
+            System.err.println("Fichier " + source + " inexistant");
+	        System.exit(1);
         }
         catch (AnalyseException ex) {
             System.err.println(ex.getMessage());
+            System.exit(1);
         }
         catch (Exception ex) {
             Logger.getLogger(Yal.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    /**
+     * Crée le nom du fichier de sortie
+     * @param source
+     * @return sortie
+     */
+    public String sortie(String source) {
+        StringBuilder sortie = new StringBuilder(10);
+        int suffixe = source.lastIndexOf('.');
+        
+        if (suffixe < 1) {
+        	sortie.append(source);
+        }
+        else {
+        	sortie.append(source.substring(0, suffixe));
+        }
+        
+        sortie.append(".mips");
+        
+        return sortie.toString();
+    }
+  
+    /**
+     * Ecriture du code MIPS
+     * @param code
+     * @param sortie
+     */
+    public void ecriture(String code, String sortie) {
+    	try {
+        	FileWriter flot = new FileWriter(sortie);
+        	BufferedWriter tampon = new BufferedWriter(flot);
+        	
+            tampon.write(code);
+        	
+            tampon.close();
+		    flot.close();          
+        }
+        catch (IOException e) { 
+        	System.err.println("Erreur lors de l'écriture du code MIPS dans le fichier " + sortie);
+	        System.exit(1);
+        }    	
+    }
+    
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Nombre incorrect d'arguments");

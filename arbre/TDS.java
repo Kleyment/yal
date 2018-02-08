fd8570c20815bd50140e45a574bcad95806e7372
Highnewb <thouveninvalentin@gmail.com>
2018-02-08 17:19:55 +0100

    Modif TDS

--- arbre/TDS.java
+++ arbre/TDS.java
@@ -1,15 +1,19 @@
 package yal.arbre;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TDS {
 
 	private static TDS ourInstance = new TDS();
 	private HashMap<Entree, Symbole> table;
-	private ArbreAbstrait arbreAbs;
+	private ArrayList<BlocDInstructions> listeBloc;
+	private int noBloc = 0;
+	private BlocDInstructions blocActuel;
 	
 	private TDS() {
 		table = new HashMap<Entree, Symbole>();
+		blocActuel = null;
 	}
 	
 	public void ajouter(Entree e, Symbole s){
@@ -22,11 +26,18 @@ public class TDS {
 	}
 	
 	public void entreeBloc(){
-		
+		BlocDInstructions newBloc = new BlocDInstructions(noBloc);
+		noBloc++;
+		listeBloc.add(newBloc);
+		blocActuel = newBloc;
 	}
 	
 	public void sortieBloc(){
-		
+		if (noBloc != 0){
+			blocActuel = listeBloc.get(noBloc - 1);
+		} else {
+			blocActuel = null;
+		}
 	}
 	
 	public static TDS getInstance() {

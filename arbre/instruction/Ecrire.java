85d7be69a44b4e9643ab13ccd2fc7c796aaa8b41
Bellanger Clément <kleyman@localhost.localdomain>
2018-02-09 09:53:06 +0100

    Modif sur Ecrire

--- arbre/instruction/Ecrire.java
+++ arbre/instruction/Ecrire.java
@@ -15,5 +15,29 @@ public abstract class Ecrire extends Instruction {
 		this.expression.verifier();		
 	}
 	
+	public String toMIPS() {
+		StringBuilder sb=new StringBuilder();
+		int hash=this.hashCode();
+		
+		if (expression.getType() == BOOLEEN) {		
+			sb.append(this.expression.toMIPS());
+			sb.append("# Ecriture d'un booléen \n");
+			sb.append("beqz $v0, alors_" + hash + "\n");
+			sb.append("la $a0, vrai\n");
+			sb.append("j end_" + hash + "\n");
+			sb.append("alors_" + hash + " :\n");
+			sb.append("la $a0, faux \n");
+			sb.append("end_" + hash + " :\n");
+			sb.append("li $v0, 4\n");
+			sb.append("syscall\n");
+		} else {
+			sb.append("# Ecriture d'un entier \n");
+			sb.append(this.expression.toMIPS());
+			sb.append("move $a0, $v0\n");
+			sb.append("li $v0, 1\n");
+			sb.append("syscall\n");
+		}
+		return sb.toString();
+	}
 
 }

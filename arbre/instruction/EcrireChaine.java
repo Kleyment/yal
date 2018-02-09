035ee41774cc4be11bb892c4bdfaeb57d04347c8
Clément 2 <clement.bellanger3@etu.univ-lorraine.fr>
2018-02-09 09:51:36 +0100

    Refactoring de Ecrire

--- arbre/instruction/EcrireChaine.java
+++ arbre/instruction/EcrireChaine.java
@@ -3,34 +3,31 @@ package yal.arbre.instruction;
 import yal.arbre.expression.Expression;
 import yal.exceptions.AnalyseSemantiqueException;
 
-public class EcrireChaine extends Ecrire{
+public class EcrireChaine extends Instruction{
 
-	public EcrireChaine(Expression expr) {
-		super(expr);
+	private String str;
+	
+	public EcrireChaine(int no, String string) {
+		super(no);
+		this.str=string;
+		
 	}
 
 	@Override
 	public void verifier(){
-		if (expression.getType() != CHAINE){
-			StringBuilder erreur = new StringBuilder(25);
-	    	
-	    	erreur.append("erreur de type :\t");
-	    	erreur.append("type de l'expression\n");
-	    	erreur.append(expression);
-	    	erreur.append("\n\t");
-	    	erreur.append("l'expression doit être une chaîne de caractères");
-	    	
-	        throw new AnalyseSemantiqueException(getNoLigne(), erreur.toString());
-		}
+		
 	}
 	
 	@Override
 	public String toMIPS() {
 		StringBuilder sb = new StringBuilder();
+		int hash=this.hashCode();
 		sb.append("# Ecriture d'une chaine \n");
-		sb.append(expression.toMIPS());
+		sb.append(".data\n");
+		sb.append("chaine_"+hash+":	.asciiz "+this.str+"\n");
+		sb.append(".text\n");
 		sb.append("li $v0, 4\n");
-		sb.append("la $a0, chaine"+expression.hashCode()+"\n");
+		sb.append("la $a0, chaine_"+hash+"\n");
 		sb.append("syscall\n");
 		return sb.toString();
 	}

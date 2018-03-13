a1bcc05b3b70c21ba1aaa6f534ff31a6414f6eb9
Pierre Génard <pierre.genard4@etu.univ-lorraine.fr>
2018-03-13 15:17:29 +0100

    entrees/sorties des blocs

---
+++ analyse/tds/Arbre.java
@@ -0,0 +1,63 @@
+package yal.analyse.tds;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import yal.analyse.tds.entree.Entree;
+import yal.analyse.tds.symbole.Symbole;
+import yal.exceptions.AnalyseSemantiqueException;
+
+public class Arbre {
+	
+	private Arbre parent;
+	private ArrayList<Arbre> fils;
+	
+	private HashMap<Entree, Symbole> table;
+	
+    
+	public Arbre() {
+		parent = null;
+		fils = new ArrayList<Arbre>();
+		table = new HashMap<Entree, Symbole>();
+	}
+	
+	public Arbre(Arbre parent) {
+		this.parent = parent;
+		fils = new ArrayList<Arbre>();
+		table = new HashMap<Entree, Symbole>();
+	}
+	
+	public void ajouter(Entree e, Symbole s, int noLigne) {
+		if (table.containsKey(e)) {
+			throw new AnalyseSemantiqueException(noLigne, "redéclaration de `" + e.getIdf() + "`");
+		}
+		
+		table.put(e, s);
+	}
+	
+	public void ajouterFils(Arbre f) {
+		fils.add(f);
+	}
+	
+	public Symbole identifier(Entree e) {
+		return table.get(e);
+	}
+	
+	public Arbre getParent() {
+		return parent;
+	}
+	
+	public int nbVariables() {
+		return table.size();
+	}
+	
+	public int tailleZoneDesVariables() {
+		return table.size() * 4;
+    }
+	
+	@Override
+	public String toString() {
+		return table.toString();
+	}
+	
+}
